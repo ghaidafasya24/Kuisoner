79df72392f8ce8076132e1d84cb88645b16c9445
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-05-07 15:07:38 +0700

    push lagiii

--- model/type.go
+++ model/type.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Responden struct {
 	ID           primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Usia         string               `bson:"usia,omitempty" json:"usia,omitempty"`
+	Usia         string             `bson:"usia,omitempty" json:"usia,omitempty"`
 	JenisKelamin string             `bson:"jenis_kelamin,omitempty" json:"jenis_kelamin,omitempty"`
 	Pekerjaan    string             `bson:"pekerjaan,omitempty" json:"pekerjaan,omitempty"`f
 }
@@ -17,5 +17,5 @@ type Pertanyaan struct {
 type Jawaban struct {
 	ID           primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 	NamaJawaban  string             `bson:"jawaban,omitempty" json:"jawaban,omitempty"`
-	TanggalJawab string `bson:"tanggal_jawab,omitempty" json:"tanggal_jawab,omitempty"`
+	TanggalJawab string 			`bson:"tanggal_jawab,omitempty" json:"tanggal_jawab,omitempty"`
 }
